8d771649d27f0b06d4dabc78a6f0e672c7abbb2b
ivank <ivankarazaev@gmail.com>
2025-03-31 23:17:15 +0300

    возможно реализован метод экспорта

--- Diplom/builder/urls.py
+++ Diplom/builder/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import save_bpmn, load_bpmn, list_diagrams, back_to_diagrams
+from .views import save_bpmn, load_bpmn, list_diagrams, back_to_diagrams, export_diagram
 
 urlpatterns = [
     path('save/', save_bpmn),
     path('load/<int:diagram_id>/', load_bpmn),
     path('list/', list_diagrams),
     path('back-to-diagrams/', back_to_diagrams, name='back_to_diagrams'),
+    path('export/', export_diagram, name='export_diagram'),
 ]
